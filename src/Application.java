ab98bd6f1296582aa2976bc2391b30bba708dba4
Ludovic Tournier <ltourn01@debian.univ-jfc.fr>
2018-02-23 14:40:28 +0100

    ajout titre a la fenetre

--- src/Application.java
+++ src/Application.java
@@ -15,6 +15,7 @@ public class Application extends Frame implements WindowListener {
 		Vue vue = new Vue();
 		modl.addObserver(vue);
 		vue.addActionListener(ctrl);
+		this.setTitle("Appli Ludo");
 		this.setLocationRelativeTo(null);
 		this.addWindowListener(this);		
 		this.add(vue);
