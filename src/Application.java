4d964e9cb0c67f6d2e543877f11ffbd15acfd032
Polina Polozyuk <ppolozyu@debian.univ-jfc.fr>
2018-02-23 14:29:48 +0100

    ajout : fermeture de l'application

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,8 @@
 import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class Application extends Frame {
+public class Application extends Frame implements WindowListener {
 
 	public static void main(String[] args) {
 		new Application();
@@ -8,16 +10,57 @@ public class Application extends Frame {
 	
 	public Application() {
 		super();
-		
 		Modèle modl = new Modèle();
 		Contrôleur ctrl = new Contrôleur(modl); 
 		Vue vue = new Vue();
 		modl.addObserver(vue);
 		vue.addActionListener(ctrl);
-		
+		this.addWindowListener(this);		
 		this.add(vue);
 		this.pack();
 		this.setVisible(true);
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		System.exit(0);
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
